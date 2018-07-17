53ae0006e543ea7472fb3be6116c7caf139bdedc
Michael Chesterton <michael.chesterton@gmail.com>
2018-07-17 19:34:14 +1000

    tidy up, use mounts for configurations

--- apache.sh
+++ apache.sh
@@ -1,47 +1,6 @@
 #!/bin/bash
 
-sleep 10 # give it sometime for initdb.sh to start and remove an old /data/initdb.ready if it exists.
-until test -f /data/nominatim-initdb.ready; do
-	echo waiting on nomintim-initdb
-	sleep 10
-done
-
 . /etc/apache2/envvars
-
-if [ ! -d /data/nominatim/apache2 ]; then
-	mkdir -p /data/nominatim
-	cp -a /etc/apache2 /data/nominatim/
-	cd /data/nominatim/apache2/sites-available && \
-		mv 000-default.conf 000-default.conf.orig &&
-		cat > 000-default.conf <<EOL
-<VirtualHost *:80>
-    DocumentRoot /Nominatim/build/website
-    ErrorLog ${APACHE_LOG_DIR}/error.log
-    CustomLog ${APACHE_LOG_DIR}/access.log combined
-    <Directory /Nominatim/build/website>
-        Options FollowSymLinks MultiViews
-        AddType text/html .php
-        DirectoryIndex search.php
-        Require all granted
-    </Directory>
-</VirtualHost>
-EOL
-
-fi
-
-if [ ! -d /data/nominatim/settings ]; then
-	mkdir -p /data/nominatim/
-	cd /Nominatim/build && \
-		mv settings /data/nominatim/
-fi
-
-cd /Nominatim/build && \
-	rm -rf settings && \
-	ln -sf /data/nominatim/settings
-
-cd /etc && \
-	rm -rf apache2 && \
-	ln -sf /data/nominatim/apache2
 
 cd ${APACHE_LOG_DIR} && \
 	ln -sf /dev/stdout access.log && \
@@ -51,4 +10,5 @@ touch /data/nominatim/nominatim.log && \
 	chgrp www-data /data/nominatim/nominatim.log && \
 	chmod g+wr /data/nominatim/nominatim.log
 
+cd /
 exec /usr/sbin/apache2 -DFOREGROUND
--- initdb.sh
+++ initdb.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 
-rm -f /data/nominatim-initdb.ready
-
 : ${OSM_PBF_URL:=http://download.geofabrik.de/australia-oceania/australia-latest.osm.pbf}
 : ${OSM_PBF:=$(basename "$OSM_PBF_URL")}
 : ${OSM_PBF_BASENAME:=$(basename "$OSM_PBF" .osm.pbf)}
 : ${OSM2PGSQLCACHE:=1000}
 
+# this is mainly so nominatim.so is available in the apache container
 if [ -d /postgres/Nominatim/build ]; then
 	rm -rf /postgres/Nominatim/build/*
 	cp -a /Nominatim/build/* /postgres/Nominatim/build
 fi
-
-until echo select 1 | gosu postgres psql template1 &> /dev/null ; do
-        echo "Waiting for postgres"
-        sleep 5
-done
 
 until echo select 1 | gosu postgres psql template1 &> /dev/null ; do
         echo "Waiting for postgres"
@@ -29,24 +23,28 @@ if [ ! -f /data/nominatim/country_name.sql ]; then
 		mkdir -p /data/nominatim && \
 		cp -a data/* /data/nominatim && \
 		chown -R postgres: /data/nominatim
+		touch /data/nominatim/nominatim.log && \
+		chgrp www-data /data/nominatim/nominatim.log && \
+		chmod g+wr /data/nominatim/nominatim.log
 fi
 
 cd /Nominatim && \
 	rm -rf data && \
 	ln -s /data/nominatim data && \
-	touch /data/nominatim/nominatim.log && \
-	chgrp www-data /data/nominatim/nominatim.log && \
-	chmod g+wr /data/nominatim/nominatim.log
 
 if ! `echo select 1 | gosu postgres psql nominatim &> /dev/null` || [ "$REDOWNLOAD" ];then
 	gosu postgres curl -L -z /Nominatim/data/country_osm_grid.sql.gz -o /Nominatim/data/country_osm_grid.sql.gz \
 		https://www.nominatim.org/data/country_grid.sql.gz
 	gosu postgres curl -L -z /data/nominatim/wikipedia_article.sql.bin -o /data/nominatim/wikipedia_article.sql.bin https://www.nominatim.org/data/wikipedia_article.sql.bin
 	gosu postgres curl -L -z /data/nominatim/wikipedia_redirect.sql.bin -o /data/nominatim/wikipedia_redirect.sql.bin https://www.nominatim.org/data/wikipedia_redirect.sql.bin
+
+	# this is a noop for a standalone nominatim container, it's used in
+	# https://githuub.com/chesty/maps-docker-compose
 	until ! test -f /data/renderd-initdb.init; do
 		echo waiting on renderd-initdb
 		sleep 5
 	done
+
 	curl -L -z /data/"$OSM_PBF" -o /data/"$OSM_PBF" "$OSM_PBF_URL"
 	curl -o /data/"$OSM_PBF".md5 "$OSM_PBF_URL".md5
 	cd /data && \
@@ -63,5 +61,3 @@ if ! `echo select 1 | gosu postgres psql nominatim &> /dev/null` || [ "$REINITDB
 		gosu postgres psql -d nominatim -f /data/nominatim/specialphrases.sql
 
 fi
-
-touch /data/nominatim-initdb.ready
