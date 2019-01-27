5779b6f925fc59964d7b703c803fa4b5fe0470aa
Michael Chesterton <michael.chesterton@gmail.com>
2019-01-27 23:47:35 +1100

    minor updates to osm-config.sh

--- osm-config.sh
+++ osm-config.sh
@@ -13,7 +13,7 @@ log_env () {
     log `env | grep -v PASSWORD`
 }
 
-log starting
+log "starting osm-config.sh"
 
 # these will only be set if they aren't already set
 : ${NPROCS:=$(grep -c ^processor /proc/cpuinfo 2>/dev/null || 1)}
@@ -28,17 +28,22 @@ log_env
 for U in osm osrm postgres root; do
     if id "$U" > /dev/null 2>&1 && [ ! -f ~"$U"/.pgpass ]; then
         if [ ! -d ~ ]; then
-            mkdir ~
+            mkdir -p ~
             chown "$U": ~
         fi
-        ( eval cd ~"$U"; touch .pgpass; chown "$U": .pgpass; chmod 600 .pgpass; \
-            echo "$POSTGRES_HOST:$POSTGRES_PORT:*:$POSTGRES_USER:$POSTGRES_PASSWORD" >> .pgpass; )
+        (
+            eval cd ~"$U"
+            touch .pgpass
+            chown "$U": .pgpass
+            chmod 600 .pgpass
+            echo "$POSTGRES_HOST:$POSTGRES_PORT:*:$POSTGRES_USER:$POSTGRES_PASSWORD" >> .pgpass
+        )
     fi
 done
 
 if [ -d /osm-config.d ]; then
     for script in /osm-config.d/*.sh; do
-        echo "$0 running $script at" `print_time`
+        log "osm-config.sh running $script"
         . "$script"
     done
 fi
