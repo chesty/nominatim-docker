1c4fc397d3d868e4f5901684e5e885178595567c
Michael Chesterton <michael.chesterton@gmail.com>
2020-06-18 21:29:36 +0000

    added bump

--- bump.sh
+++ bump.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
-if git status | grep -q 'modified:'; then
-    echo "modified files detected, commit or stash and rerun"
-    exit 1
-fi 
+set -x
 
-DATE=`date +%y.%m.%d.1`
+for b in master 12; do
+	git checkout $b
 
-sed -i "s/ENV BUMP .*/ENV BUMP $DATE/" Dockerfile 
-git stage Dockerfile
-git commit -m "bump $DATE"
-git push origin 12
-git push origin master
+	if git status | grep -q 'modified:'; then
+    	echo "modified files detected, commit or stash and rerun"
+    	exit 1
+	fi 
 
+	DATE=`date +%y.%m.%d.1`
+
+	sed -i "s/ENV BUMP .*/ENV BUMP $DATE/" Dockerfile 
+	git stage Dockerfile
+	git commit -m "bump $DATE"
+	git push origin $b
+done
